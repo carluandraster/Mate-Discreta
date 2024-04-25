af88d451454f56e02bb3523bf703494e67d27623
carluandraster <99754800+carluandraster@users.noreply.github.com>
2024-04-25 18:17:27 -0300

    Add files via upload

--- Division entera/division.pas
+++ Division entera/division.pas
@@ -1,5 +1,29 @@
 Program divisionEntera;
 
+function techo (x:real):integer;
+begin
+    if x=trunc(x) then
+        techo:=trunc(x)
+    else
+        if x>0 then
+            techo:=trunc(x+1)
+        else
+            if x<0 then
+                techo:=trunc(x);
+end;
+
+function piso (x:real):integer;
+begin
+    if x=trunc(x) then
+        piso:=trunc(x)
+    else
+        if x>0 then
+            piso:=trunc(x)
+        else
+            if x<0 then
+                piso:=trunc(x-1);
+end;
+
 procedure ingresarDatos(var dividendo,divisor:integer);
 begin
     writeLn('Ingrese dividendo: ');
@@ -11,16 +35,28 @@ end;
     
 
 var
-    dividendo,divisor:integer;
+    dividendo,divisor,cociente,resto:integer;
 
 begin
     ingresarDatos(dividendo,divisor);
     while divisor<>0 do
     begin
-        // Imprimir respuestas
-        writeLn('Cociente entero: ',dividendo div divisor);
-        writeLn('Resto: ',dividendo mod divisor);
+        // Calcular cociente y resto
+        if divisor>0 then
+        begin
+            cociente:=piso(dividendo/divisor);
+        end
+        else
+        begin
+            cociente:=techo(dividendo/divisor);
+        end;
+        resto:=dividendo-cociente*divisor;
 
+        // Imprimir resultados
+        writeLn('Cociente entero: ',cociente);
+        writeLn('Resto: ',resto);
+
+        // Pedir datos para la siguiente división
         ingresarDatos(dividendo,divisor);
     end;
 end.
